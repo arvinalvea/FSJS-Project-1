3271572cdfaee5e4f755e625e5d76ee011612949
Arvin Alvea <arvinalvea@gmail.com>
2021-05-05 11:22:09 -0400

    Update script.js
    Added changeBackgroundColor function.
    Added auto-refresh function with setInterval()

--- a_random_quote_generator-v1/js/script.js
+++ a_random_quote_generator-v1/js/script.js
@@ -8,13 +8,14 @@ project 1 - A Random Quote Generator
 // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
 
 /***
- * `quotes` array
+ * Array of objects containing the quotes, their source and/or citation, year
  ***/
 let quotes = [
     {
         quote:
             "All our dreams can come true, if we have the courage to pursue them.",
         source: "Walt Disney",
+        tag: "Motivational",
     },
     {
         quote: "The secret of getting ahead is getting started.",
@@ -39,19 +40,37 @@ let quotes = [
 
 /***
  * `getRandomQuote` function
+ *
+ * This function will generate a random number between zero and -1 the length of the quotes array.
+ * This number will be used as the index to access a random object within the quotes array and returns that object.
  ***/
 
 function getRandomQuote() {
     let randomNumber = Math.floor(Math.random() * quotes.length);
 
-    // console.log(randomNumber);
-    // console.log(quotes[randomNumber]);
-
     return quotes[randomNumber];
 }
 
 /***
+ * `changeBackgroundColor` function
+ *
+ * This function will change the background color when a new quote is printed to the page.
+ ***/
+
+function changeBackgroundColor() {
+    let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+}
+
+/***
  * `printQuote` function
+ *
+ * This function will call on the `getRandomQuote` function and builds a HTML string using the properties of the
+ * object returned from the `getRandomQuote` function.
+ *
+ * It will check if the object has extra properties and list them accordingly.
+ *
+ * It will then access the element in the DOM with an ID of `quote-box` and insert the HTML it just built.
  ***/
 
 function printQuote() {
@@ -71,13 +90,23 @@ function printQuote() {
         `;
     }
 
+    if (randomQuote.tag) {
+        quoteHTML += `
+      <span>, ${randomQuote.tag} </span>
+      `;
+    }
+
     quoteHTML += ` </p>`;
     console.log(quoteHTML);
-
-    return quoteHTML;
+    changeBackgroundColor();
+    return (document.getElementById("quote-box").innerHTML = quoteHTML);
 }
-let newQuote = printQuote();
+
+newQuote = printQuote();
 document.getElementById("quote-box").innerHTML = newQuote;
+setInterval(function () {
+    printQuote();
+}, 10000);
 
 /***
  * click event listener for the print quote button
